4a1380f46de7be686ba8390e89594062b86145c7
Mazeraser <68272519+Mazeraser@users.noreply.github.com>
2022-11-04 21:58:35 +0300

    0.1.3

--- scripts/dialogueWindow.gd
+++ scripts/dialogueWindow.gd
@@ -2,8 +2,7 @@ extends Control
 
 export (float) var textSpeed = 1
 
-onready var text = get_parent().get_node("base").intro
-
+var text
 var dialogue_index = 0
 var active
 var finished
@@ -14,11 +13,11 @@ func text_read():
 		active = true
 		finished = false
 		
-		if text[dialogue_index]["choise"] != "" and text[dialogue_index]["choise"] != "Выстрел":
-			$ColorRect/Button.text = text[dialogue_index]["choise"]
-		elif text[dialogue_index]["choise"] == "Выстрел":
+		if text[dialogue_index]["choise"] == "Выстрел":
 			print("SHOOT!")
 			shoot = true
+		elif text[dialogue_index]["choise"] == "Катапультироваться.":
+			get_tree().change_scene("res://levels/scenes.tscn")
 		
 		$Background.texture = text[dialogue_index]["Background"]
 		$ColorRect/Label.text = text[dialogue_index]["Name"]
@@ -31,11 +30,6 @@ func text_read():
 		active = false
 		finished = true
 	
-	if $ColorRect/Button.text == "":
-		$ColorRect/Button.visible = false
-	else:
-		$ColorRect/Button.visible = true
-	
 	if $ColorRect/RichTextLabel.text == "":
 		$ColorRect.visible = false
 	else:
@@ -43,22 +37,18 @@ func text_read():
 	$ColorRect/RichTextLabel.percent_visible = 0
 
 func _ready():
+	if GS.escape:
+		text = get_parent().get_node("base").after_catapult
+	else:
+		text = get_parent().get_node("base").intro
 	text_read()
 func _process(delta):
 	
 	if active:
 		$ColorRect/RichTextLabel.percent_visible += delta * float(10) * textSpeed
-		if Input.is_action_just_pressed("ui_accept") and $ColorRect/Button.visible == false and shoot == false:
+		if Input.is_action_just_pressed("ui_accept") and shoot == false:
 			text_read()
 		elif shoot == true:
 			if Input.is_action_just_pressed("lkm"):
 				shoot = false
 				text_read()
-
-
-func _on_Button_pressed():
-	if $ColorRect/Button.text == "Катапультироваться.":
-		text = get_parent().get_node("base").after_catapult
-		dialogue_index = 0
-		$ColorRect/Button.text = ""
-		text_read()
